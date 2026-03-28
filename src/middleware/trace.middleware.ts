512b5e8baf8ce73f206c821c97b6697644e95d12
rohit <153482419+mr-saadhak@users.noreply.github.com>
2026-03-28 18:14:53 +0530

    feat: traceId for every request

---
+++ src/middleware/trace.middleware.ts
@@ -0,0 +1,11 @@
+import { randomUUID } from "node:crypto";
+import { Request, Response, NextFunction } from "express";
+
+export const traceMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    const traceId = randomUUID();
+
+    req.traceId = traceId;
+    res.setHeader("X-Trace-Id", traceId);
+
+    next();
+}
