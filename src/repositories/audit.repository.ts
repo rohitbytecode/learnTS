d5079e7d0e90f3b6f1f4cd60bc247f2c3c9862de
rohit <153482419+mr-saadhak@users.noreply.github.com>
2026-03-28 20:47:54 +0530

    feat: audit logs store in db

---
+++ src/repositories/audit.repository.ts
@@ -0,0 +1,27 @@
+import { randomUUID } from "node:crypto";
+import { pool } from "@/config/db";
+
+interface AuditLog {
+    action: string;
+    userId?: string;
+    traceId?: string;
+    metadata?: Record<string, any>;
+}
+
+export const insertAuditLog = async ({
+    action,
+    userId,
+    traceId,
+    metadata,
+}: AuditLog) => {
+    const query = `INSERT INTO audit_logs (id, action, user_id, trace_id, metadata)
+    VALUES ($1, $2, $3, $4, $5)`;
+
+    await pool.query(query, [
+        randomUUID(),
+        action,
+        userId || null,
+        traceId || null,
+        metadata || {},
+    ]);
+};
